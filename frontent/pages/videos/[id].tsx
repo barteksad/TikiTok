fcdf68a5e18d7f3b2e8fd30da977671b74e01ec3
barteksad <barteksadlej@gmail.com>
2023-01-05 11:31:05 +0100

    improved css, added likes basics

--- frontent/pages/videos/[id].tsx
+++ frontent/pages/videos/[id].tsx
@@ -1,23 +1,39 @@
+import { useState, useEffect } from "react";
+import Image from 'next/image'
 import styles from "../../styles/Video.module.css";
 
 type Props = {
-    id: string;
+  id: string;
 }
 
 const Video = ({ id }: Props) => {
-    const url = `https://iframe.mediadelivery.net/embed/83596/${id}?autoplay=true&loop=true`;
+  const [like, setLike] = useState(false);
+  const [likesCount, setLikesCount] = useState(0);
+  const handleLikeClikc = async () => {
+    setLike(!like);
+  }
 
-    return (
-        <div className={styles.div}>
-          <iframe
-            className={styles.iframe}
-            src={url}
-            loading="lazy"
-            allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
-            allowFullScreen={true}>
-          </iframe>
-        </div>
-    )
+  const url = `https://iframe.mediadelivery.net/embed/83596/${id}?autoplay=true&loop=true`;
+
+  return (
+    <div className={styles.videoDiv}>
+      <iframe
+        className={styles.videoIframe}
+        src={url}
+        loading="lazy"
+        allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+        allowFullScreen={true}>
+      </iframe>
+      <div className={styles.likeButton} onClick={handleLikeClikc}>
+        <Image 
+        src={like ? "/heart-filled.svg" : "/heart-notfilled.svg" }
+        alt={""} 
+        width={100} 
+        height={100}></Image>
+        <p className={styles.likesCount}>{likesCount}</p>
+      </div>
+    </div>
+  )
 }
 
 export default Video;
